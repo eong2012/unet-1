d6041a00631c0c4c81657637e5f9124065b5ec22
Sam Russell <sam.h.russell@gmail.com>
2017-08-12 19:10:31 +1200

    working towards N-steps-per-tile approach

--- test_image2.py
+++ test_image2.py
@@ -23,30 +23,49 @@ width, height = scaled_image.size
 
 scaled_image_data = np.asarray(image, dtype='float32') / 255.
 
+tile_pixels = 32
+step_pixels = 8
+overlap_count = (tile_pixels / step_pixels)
+overlap_area = overlap_count * overlap_count * 1.0
+
 rows = []
-for y in xrange(0, height-16, 16):
+for y in xrange(0, height-(tile_pixels-step_pixels), step_pixels):
   cells = []
-  for x in xrange(0, width-16, 16):
-    tile = scaled_image_data[y:y+32, x:x+32, :]
+  for x in xrange(0, width-(tile_pixels-step_pixels), step_pixels):
+    tile = scaled_image_data[y:y+tile_pixels, x:x+tile_pixels, :]
     output = unet.model.predict(np.array([tile]))
-    output_data = (output * (255. / 4.)).astype('uint8')
+    output_data = (output * (255. / overlap_area)).astype('uint8')
     cells.append(output_data[0])
 
   # recombine
-  even_cells = [cells[i] for i in xrange(0, len(cells), 2)]
-  odd_cells = [cells[i] for i in xrange(1, len(cells), 2)]
+  even_cells = [cells[i] for i in xrange(0, len(cells), overlap_count)]
+  sub1_cells = [cells[i] for i in xrange(1, len(cells), overlap_count)]
+  sub2_cells = [cells[i] for i in xrange(2, len(cells), overlap_count)]
+  sub3_cells = [cells[i] for i in xrange(3, len(cells), overlap_count)]
   even_row = np.concatenate(even_cells, axis=1)
-  odd_row = np.concatenate(odd_cells, axis=1)
-  odd_row = np.pad(odd_row, ((0,0), (16,16), (0,0)), 'constant')
-  row = even_row + odd_row
+  sub1_row = np.concatenate(sub1_cells, axis=1)
+  sub2_row = np.concatenate(sub2_cells, axis=1)
+  sub3_row = np.concatenate(sub3_cells, axis=1)
+  sub1_row = np.pad(sub1_row, ((0,0), (step_pixels*1,step_pixels*3), (0,0)), 'constant')
+  sub2_row = np.pad(sub2_row, ((0,0), (step_pixels*2,step_pixels*2), (0,0)), 'constant')
+  sub3_row = np.pad(sub3_row, ((0,0), (step_pixels*3,step_pixels*1), (0,0)), 'constant')
+  row = even_row + sub1_row + sub2_row + sub3_row
   rows.append(row)
 
 # recombine
-even_rows = [rows[i] for i in xrange(0, len(rows), 2)]
-odd_rows = [rows[i] for i in xrange(1, len(rows), 2)]
+even_rows = [rows[i] for i in xrange(0, len(rows), overlap_count)]
+sub1_rows = [rows[i] for i in xrange(1, len(rows), overlap_count)]
+sub2_rows = [rows[i] for i in xrange(2, len(rows), overlap_count)]
+sub3_rows = [rows[i] for i in xrange(3, len(rows), overlap_count)]
 even_image_data = np.concatenate(even_rows, axis=0)
-odd_image_data = np.concatenate(odd_rows, axis=0)
-odd_image_data = np.pad(odd_image_data, ((16,16), (0,0), (0,0)), 'constant')
-full_image_data = even_image_data + odd_image_data
+sub1_image_data = np.concatenate(sub1_rows, axis=0)
+sub2_image_data = np.concatenate(sub2_rows, axis=0)
+sub3_image_data = np.concatenate(sub3_rows, axis=0)
+sub1_image_data = np.pad(sub1_image_data, ((step_pixels*1,step_pixels*3), (0,0), (0,0)), 'constant')
+sub2_image_data = np.pad(sub2_image_data, ((step_pixels*2,step_pixels*2), (0,0), (0,0)), 'constant')
+sub3_image_data = np.pad(sub3_image_data, ((step_pixels*3,step_pixels*1), (0,0), (0,0)), 'constant')
+# square root to pull things up towards 1.0
+full_image_data = even_image_data + sub1_image_data + sub2_image_data + sub3_image_data
+full_image_data = np.sqrt(full_image_data / 255.) * 255.
 output_image = Image.fromarray(full_image_data[:,:,0]).convert("RGB")
 output_image.save("beetil1_heatmap%dx%d.png" % (width, height))
